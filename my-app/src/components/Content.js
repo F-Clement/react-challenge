0f74534cd729e131e6962d1ff90078930dfb4b39
F-Clement <>
2023-12-28 00:10:04 +0000

    Component Lifecycles challenge done

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,34 +1,51 @@
-import React, { Component } from 'react'
-import css from "./css/Content.module.css"
-import {savedPosts} from "../posts.json"
-import PostItem from './PostItem'
+import React, { Component } from 'react';
+import css from "./css/Content.module.css";
+import { savedPosts } from "../posts.json";
+import PostItem from './PostItem';
+import Loader from './Loader';
 
 export class Content extends Component {
     constructor(props) {
-      super(props)
-    
-      this.state = {
-        
-      }
+        super(props);
+
+        this.state = {
+            isLoaded: false
+        };
     }
 
+    componentDidMount() {
 
-  render() {
-    return (
-      <div className={css.Content}>
-        <div className={css.TitleBar}>
-            <h1>My Photos</h1>
-        </div>
-        <div className={css.SearchResults}>
-            {savedPosts.map(post => {
-                return <PostItem key={post.title} post={post}/>
-            })}
-            
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true
+            });
+        }, 2000);
+    };
 
-        </div>
-      </div>
-    )
-  }
+
+    render() {
+        return (
+            <div className={css.Content}>
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                </div>
+                <div className={css.SearchResults}>
+                    {savedPosts.map(post => {
+                        return (
+                            <div>
+                                {this.state.isLoaded && <PostItem key={post.title} post={post} />}
+                                {this.state.isLoaded && !<Loader key={1} />}
+
+                            </div>
+                        );
+                    })}
+
+
+                </div>
+                <Loader />
+            </div>
+        );
+    }
 }
 
-export default Content
+export default Content;
